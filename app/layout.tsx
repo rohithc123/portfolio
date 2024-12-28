2bc63282fac3e7124a70534923593755d67b51fc
Rohithc123 <rohithc.eec.cseb@gmail.com>
2024-12-28 11:44:19 +0530

    feat:Initial Setup

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Rohith C Portfolio",
+  description:
+    "Crafting seamless web experiences with expertise in both front-end design and back-end development",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html>
+      <body>
+        <div>
+          <h1>Hello,Portfolio</h1>
+        </div>
+      </body>
+    </html>
+  );
+}
