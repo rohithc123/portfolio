ed13f799359de8ca518f98bf896143fc6d419d79
Rohithc123 <rohithc.eec.cseb@gmail.com>
2024-12-30 23:51:29 +0530

    fix:mitigating text not visible in the home page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,21 +17,21 @@ const Hero = () => {
           fill="purple"
         />
         <Spotlight className="-top-28 left-80 h-[80vh] w-[50vw] " fill="blue" />
-        <div className="h-screen w-full dark:bg-black-100 bg-black-100  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] flex items-center justify-center absolute  top-0 left-0">
+        <div className="-z-10 h-screen w-full dark:bg-black-100 bg-black-100  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] flex items-center justify-center absolute  top-0 left-0">
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         </div>
       </div>
 
       <div className="flex justify-center text-white">
         <div className=" max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h2 className=" uppercase tracking-widest text-xs text-center   max-w-80 z-10">
+          <h2 className=" uppercase tracking-widest text-xs text-center   max-w-80 ">
             Dynamic Web Magic with Next.js
           </h2>
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
             words="Crafting ideas into digital solutions."
           />
-          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl z-10">
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl ">
             Hi, I&apos;m Rohith, a Full Stack Developer{" "}
           </p>
           <a href="#about">
