0fb9cc989bbb356e17c48dbcb2de41079a3d0580
Rohithc123 <rohithc.eec.cseb@gmail.com>
2024-12-31 17:57:09 +0530

    feat:Changes to navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
 import Image from "next/image";
 import Hero from "../components/Hero";
+import Grid from "../components/Grid";
+import RecentProjects from "../components/RecentProjects";
+import { FloatingNav } from "./../components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
+import { navItems} from "@/data";
+// import { hydrateRoot } from "react-dom/client";
+
 
 export default function Home() {
+  // hydrateRott(document,<RecentProjects/>)
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={navItems}
+        />
         <Hero />
+        <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
