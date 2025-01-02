bee8533d19c6f7b8b8b3d32adfb74a430f62c503
Rohithc123 <rohithc.eec.cseb@gmail.com>
2025-01-02 10:34:50 +0530

    feat:Content to show in website

---
+++ data/index.ts
@@ -0,0 +1,181 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Full stack Website",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to work together with me?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Kratos 23 - Symposium Website",
+    des: "Kratos '23, organized by Easwari Engineering College, is an annual tech and cultural fest celebrating innovation, creativity, and talent.",
+    img: "/kratos23-figma.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/fm.svg"],
+    link: "https://github.com/nithssh/kratos23",
+    imgClassName: "bg-black-100",
+  },
+  {
+    id: 2,
+    title: "Learnify - E Learning Platform",
+    des: "Leanify is a cutting-edge e-learning platform built with React, Tailwind CSS, and Express, offering features like admin and user dashboards, file uploads, and video content management.",
+    img: "/learnify.svg",
+    iconLists: [
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/express.svg",
+      "/vercel.svg",
+      "/figma.svg",
+    ],
+    link: "https://www.figma.com/design/KY2TZHiutd2iEPXJjSlPmb/Intern-project?node-id=0-1&t=fU3U0TA3dMzPkKRs-1",
+  },
+  {
+    id: 3,
+    title: "TIC - Think India Club IIT Roorkee",
+    des: "Think India is a pan-India initiative that unites students from premier institutions like IITs, IIMs, IISc, and NLUs to foster a 'Nation First' mindset. The platform encourages young leaders to deliberate on national issues and develop innovative solutions for India's progress. ",
+    img: "/tic.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/fm.svg"],
+    link: "https://github.com/rohithc123/TIC",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Full Stack Developer",
+    desc: "During my full-stack web development internship, I gained hands-on experience building dynamic and scalable web applications using React.js , Next.js  and Express for backend API management.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Backend Developer",
+    desc: "As a backend developer working with MS SQL, I focused on managing and optimizing SQL queries to enhance data processing and system performance. I applied principles from the theory of computations to design efficient algorithms and streamline database operations for the company.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Technical Head of Coder's Club",
+    desc: "As the technical head of the Coder Club at my college, I led a team of developers, fostering collaboration and enhancing my leadership, teamwork, and communication skills. This role allowed me to guide projects, coordinate events, and mentor members.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Full stack developer for multiple clubs",
+    desc: "I currently serve as the Joint Secretary of Web Development in the TIC Club and as a website developer for the IR Cell and CIG Cell at IIT Roorkee, contributing to the development and maintenance of their websites",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/rohithc123",
+  },
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/rohith-c-b1335720b/",
+  },
+];
