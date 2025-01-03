963141ee604e03baaf56175b41664c0adfd56e36
Rohithc123 <rohithc.eec.cseb@gmail.com>
2025-01-03 21:17:17 +0530

    fix:Removing Giest import error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import Providers from "../components/ThemeProvider";
