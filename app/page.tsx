b94437f8c4ea8d639f8419e505f280e27ce2a687
Rohithc123 <rohithc.eec.cseb@gmail.com>
2025-01-04 08:26:43 +0530

    fix:document is not defined

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 "use client";
 import Image from "next/image";
-import Hero from "../components/Hero";
-import Grid from "../components/Grid";
-import Experience from "../components/Experience";
-import RecentProjects from "../components/RecentProjects";
-import { FloatingNav } from "./../components/ui/FloatingNav";
+// import Hero from "../components/Hero";
+// import Grid from "../components/Grid";
+// import Experience from "../components/Experience";
+// import RecentProjects from "../components/RecentProjects";
+// import { FloatingNav } from "./../components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
 import { navItems } from "@/data";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
+
+import { dynamic } from "next/dynamic";
+const Hero = dynamic(() => import("../components/Hero"));
+const Grid = dynamic(() => import("../components/Grid"));
+const Experience = dynamic(() => import("../components/Experience"));
+const RecentProjects = dynamic(() => import("../components/RecentProjects"));
+const FloatingNav = dynamic(() => import("../components/ui/FloatingNav"));
+const Footer = dynamic(() => import("../components/Footer"));
 
 export default function Home() {
   return (
