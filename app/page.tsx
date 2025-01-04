6ab6202a1945e16893720f38a3b94a1ee65663d6
Rohithc123 <rohithc.eec.cseb@gmail.com>
2025-01-04 07:35:59 +0530

    fix:fixing document not defined err

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,24 @@
+"use client";
 import Image from "next/image";
 import Hero from "../components/Hero";
 import Grid from "../components/Grid";
-import Experience from "../components/Experience"
+import Experience from "../components/Experience";
 import RecentProjects from "../components/RecentProjects";
 import { FloatingNav } from "./../components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
-import { navItems} from "@/data";
-import Footer from "../components/Footer"
+import { navItems } from "@/data";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={navItems}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
-          <Experience/>
-          <Footer/>
+        <Experience />
+        <Footer />
       </div>
     </main>
   );
