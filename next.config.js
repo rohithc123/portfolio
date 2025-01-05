9c1f086b8aca432f0bf51b9051f1c3e476824f8c
Rohithc123 <rohithc.eec.cseb@gmail.com>
2025-01-05 08:57:09 +0530

    fix:Removing ssr

--- next.config.js
+++ next.config.js
@@ -1,8 +1,27 @@
 // import type { NextConfig } from "next";
+// import nextConfig from "./next.config";
 
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// };
+
+// export default nextConfig;
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
+  },
+
+  output: "standalone",
+  async rewrites() {
+    return [
+      // Rewrite everything to `pages/index`
+      {
+        source: "/:any*",
+        destination: "/",
+      },
+    ];
   },
 };
 
